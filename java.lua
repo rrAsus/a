be111d8387ca49bbff9ae2ba8689339852e706bd
rrAsus <102998736+rrAsus@users.noreply.github.com>
2023-03-25 19:14:01 +0100

    Update java.lua

--- java.lua
+++ java.lua
@@ -1,65 +1,93 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Javolo Hub TDS", "DarkTheme")
-local Script = Window:NewTab("Scripts")
-local turn = false
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Javolo Hub TDS", HidePremium = false, IntroEnabled = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
-local Baker = Script:NewSection("Scripts & Abilities")
-Baker:NewButton("Commander Chain", "Place 3 commanders lvl 2", function(txt)
-	local args = {
-		loadstring(game:HttpGet("https://mmdevelopment.xyz/scripts/tdsautochain"))()
-	}
-	game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer(unpack(args))
-end)
+local Tab = Window:MakeTab({
+	Name = "General",
+	Icon = "rbxassetid://",
+	PremiumOnly = false
+})
 
-Baker:NewButton("Skip with keybind E", "Press E to skip", function(txt)
-	local args = {
-		game:GetService("UserInputService").InputBegan:Connect(function(input, chatting)
-if input.KeyCode == Enum.KeyCode.E and not chatting then
-game.ReplicatedStorage.RemoteEvent:FireServer("Waves","Skip")
-end
-end)
-}
-	game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer(unpack(args))
-end)
+local Section = Tab:AddSection({
+	Name = "Scripts"
+})
 
-Baker:NewButton("Auto Medic Macro", "Uses medics abillity", function(txt)
-	local args = {
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/TacoCatBackWardsIsTacoCat/scripts/main/TDS%20Auto%20Medic.lua",true))()
-	}
-	game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer(unpack(args))
-end)
+Tab:AddButton({
+	Name = "Auto Commander Chain",
+	Callback = function()
+        loadstring(game:HttpGet("https://mmdevelopment.xyz/scripts/tdsautochain"))()
+      		print("button pressed")
+  	end    
+})
 
-Baker:NewButton("Sell farms at 40 or 50", "Depends on what you choose", function(txt)
-	local args = {
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/rrAsus/d/main/d",true))()
-	}
-	game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer(unpack(args))
-end)
+Tab:AddButton({
+	Name = "Auto Medic Macro",
+	Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/TacoCatBackWardsIsTacoCat/scripts/main/TDS%20Auto%20Medic.lua",true))()
+      		print("button pressed")
+  	end    
+})
 
-Baker:NewButton("Stacker V3", "Upgrader", function(txt)
-	local args = {
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/rrAsus/b/main/b.lua",true))()
-	}
-	game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer(unpack(args))
-end)
+Tab:AddButton({
+	Name = "Skip With E",
+	Callback = function()
+        game:GetService("UserInputService").InputBegan:Connect(function(input, chatting)
+            if input.KeyCode == Enum.KeyCode.E and not chatting then
+            game.ReplicatedStorage.RemoteEvent:FireServer("Waves","Skip")
+            end
+            end)
+      		print("button pressed")
+  	end    
+})
 
-Baker:NewButton("Infinite Yield", "Cmd box", function(txt)
-	local args = {
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source",true))()
-	}
-	game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer(unpack(args))
-end)
+Tab:AddButton({
+	Name = "Stacker V3",
+	Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/rrAsus/b/main/b.lua",true))()
+      		print("button pressed")
+  	end    
+})
 
-Baker:NewButton("CMDX", "Cmd box", function(txt)
-	local args = {
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source",true))()
-	}
-	game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer(unpack(args))
-end)
+Tab:AddButton({
+	Name = "Sell Farms At 40 , 50",
+	Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/rrAsus/d/main/d",true))()
+  	end    
+})
 
-Baker:NewButton("Player Stats UI TDS", "Useful for Viewing", function(txt)
-	local args = {
+Tab:AddButton({
+	Name = "Infinite Yield FE",
+	Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source",true))()
+  	end    
+})
+
+Tab:AddButton({
+	Name = "CMD X FE",
+	Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source",true))()
+  	end    
+})
+
+Tab:AddButton({
+	Name = "Player Stats ",
+	Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/rrAsus/c/main/c.lua",true))()
-	}
-	game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer(unpack(args))
-end)
+      		print("button pressed")
+  	end    
+})
+
+local Tab2 = Window:MakeTab({
+	Name = "Credits",
+	Icon = "rbxassetid://",
+	PremiumOnly = false
+})
+
+local Section = Tab2:AddSection({
+	Name = "Credits"
+})
+
+Tab2:AddLabel("Script Owner : FakeBadger (Java) or (Javolo)")
+Tab2:AddLabel("Testing : ClozzMane")
+Tab2:AddLabel("And You For Using It")
+
+OrionLib:Init()
